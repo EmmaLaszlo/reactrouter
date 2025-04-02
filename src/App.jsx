c2db7206e3f807dce09bbaa70ac5443519221015
Emma Grace <emmalaslo@gmail.com>
2025-04-02 19:36:05 -0400

    home page created

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,24 @@ import './App.css'
 import { Routes, Route, Link } from 'react-router-dom'
 import Blue from './components/Blue'
 import Red from './components/Red'
+import Home from './components/Home' // add this with your other imports
+
 
 function App() {
   return (
     <div id="container">
       <div id="navbar">
-        <Link to="/blue">blue</Link>
-        <Link to="/red">red</Link>
-      </div>
+  <Link to="/">home</Link>
+  <Link to="/blue">blue</Link>
+  <Link to="/red">red</Link>
+</div>
+
 
       <div id="main-section">
         <Routes>
           <Route path="/blue" element={<Blue />} />
           <Route path="/red" element={<Red />} />
+          <Route path="/" element={<Home />} />
         </Routes>
       </div>
     </div>
