23ed91d22164d02a767bcd8dc9042b1aa073a907
Emma Grace <emmalaslo@gmail.com>
2025-04-03 16:47:43 -0400

    styling and some more routes added, also split more components

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,14 @@
 import './App.css'
-import { Routes, Route, Link } from 'react-router-dom'
-import Blue from './components/Blue'
-import Red from './components/Red'
-import Home from './components/Home' // add this with your other imports
-
+import Navbar from './components/Navbar'
+import Main from './components/Main'
+import Footer from './components/Footer'
 
 function App() {
   return (
     <div id="container">
-      <div id="navbar">
-  <Link to="/">home</Link>
-  <Link to="/blue">blue</Link>
-  <Link to="/red">red</Link>
-</div>
-
-
-      <div id="main-section">
-        <Routes>
-          <Route path="/blue" element={<Blue />} />
-          <Route path="/red" element={<Red />} />
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </div>
+      <Navbar />
+      <Main />
+      <Footer />
     </div>
   )
 }
---
+++ src/components/Main.jsx
@@ -0,0 +1,18 @@
+import { Routes, Route } from 'react-router-dom'
+import Home from './Home'
+import Blue from './Blue'
+import Red from './Red'
+import Yellow from './Yellow'
+
+export default function Main() {
+  return (
+    <div id="main-section">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/blue" element={<Blue />} />
+        <Route path="/red" element={<Red />} />
+        <Route path="/yellow" element={<Yellow />} />
+      </Routes>
+    </div>
+  )
+}
